149f16816b1b39ae633befdd0d595a853339d8de
erandis-vol <jack.bulan@gmail.com>
2018-03-10 10:38:35 -0500

    Add types

---
+++ binaryen.NET.Tests/TypeTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binaryen.Tests
+{
+    [TestClass]
+    public class TypeTests
+    {
+        const uint None = 0;
+        const uint Int32 = 1;
+        const uint Int64 = 2;
+        const uint Float32 = 3;
+        const uint Float64 = 4;
+        const uint Unreachable = 5;
+        const uint Auto = 0xFFFFFFFF; // (uint32_t)(-1)
+
+        [TestMethod]
+        public void TestNone()
+        {
+            Assert.AreEqual(Type.None, None);
+        }
+
+        [TestMethod]
+        public void TestInt32()
+        {
+            Assert.AreEqual(Type.Int32, Int32);
+        }
+
+        [TestMethod]
+        public void TestInt64()
+        {
+            Assert.AreEqual(Type.Int64, Int64);
+        }
+
+        [TestMethod]
+        public void TestFloat32()
+        {
+            Assert.AreEqual(Type.Float32, Float32);
+        }
+
+        [TestMethod]
+        public void TestFloat64()
+        {
+            Assert.AreEqual(Type.Float64, Float64);
+        }
+
+        [TestMethod]
+        public void TestUnreachable()
+        {
+            Assert.AreEqual(Type.Unreachable, Unreachable);
+        }
+
+        [TestMethod]
+        public void TestAuto()
+        {
+            Assert.AreEqual(Type.Auto, Auto);
+        }
+    }
+}
---
+++ binaryen.NET/Type.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Binaryen
+{
+    public static class Type
+    {
+        /// <summary>No type.</summary>
+        public static readonly uint None;
+        /// <summary>32-bit integer.</summary>
+        public static readonly uint Int32;
+        /// <summary>64-bit integer.</summary>
+        public static readonly uint Int64;
+        /// <summary>32-bit floating point number.</summary>
+        public static readonly uint Float32;
+        /// <summary>64-bit floating point number.</summary>
+        public static readonly uint Float64;
+        /// <summary>Special type. Used to indicate unreachable code.</summary>
+        public static readonly uint Unreachable;
+        /// <summary>Special type. Used as the type of a block to allow the API determine it automatically.</summary>
+        public static readonly uint Auto;
+
+        static Type()
+        {
+            None = BinaryenTypeNone();
+            Int32 = BinaryenTypeInt32();
+            Int64 = BinaryenTypeInt64();
+            Float32 = BinaryenTypeFloat32();
+            Float64 = BinaryenTypeFloat64();
+            Unreachable = BinaryenTypeUnreachable();
+            Auto = BinaryenTypeAuto();
+        }
+
+        #region Imports
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeNone();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeInt32();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeInt64();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeFloat32();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeFloat64();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeUnreachable();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint BinaryenTypeAuto();
+
+        #endregion
+    }
+}
