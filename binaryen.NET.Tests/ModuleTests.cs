85e5e18ccce7bec5526df5e117092e19948f4238
erandis-vol <jack.bulan@gmail.com>
2018-03-10 10:16:50 -0500

    Added test project

---
+++ binaryen.NET.Tests/ModuleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Binaryen.Tests
+{
+    [TestClass]
+    public class ModuleTests
+    {
+        [TestMethod]
+        public void TestCreateAndDestroy()
+        {
+            var module = new Module();
+            module.Dispose();
+        }
+    }
+}
---
+++ binaryen.NET.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("binaryen.NET.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("binaryen.NET.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2b136573-310b-48e5-af7c-dd13b7c1c1ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
