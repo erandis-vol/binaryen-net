42b008a57fdeafccdffc48314621b66cf4628c75
erandis-vol <jack.bulan@gmail.com>
2018-03-09 19:03:29 -0500

    Started Module

--- binaryen.NET/Module.cs
+++ binaryen.NET/Module.cs
@@ -1,13 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Binaryen
 {
-    public class Module
+    public class Module : IDisposable
     {
+        private IntPtr handle;
 
+        public Module()
+        {
+            handle = BinaryenModuleCreate();
+        }
+
+        ~Module()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (handle != IntPtr.Zero)
+            {
+                BinaryenModuleDispose(handle);
+                handle = IntPtr.Zero;
+            }
+        }
+
+        #region Imports
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr BinaryenModuleCreate();
+
+        [DllImport("binaryen", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void BinaryenModuleDispose(IntPtr handle);
+
+        #endregion
     }
 }
